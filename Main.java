6bbf47f43a91d716fd10e01a17d2048c2b6895f0
Muxamch <Muxamch@gmail.com>
2020-07-13 15:21:01 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        double votesA = 1548;
-        double votesB = 733;
+        double votesA = 5;
+        double votesB = 5;
         double difference = Double.max(votesA,votesB) - Double.min(votesA,votesB);
         double result = 100 / (votesA + votesB) * difference;
-        System.out.printf("%.4f",result);
+        if (result == 50 || (votesA == votesB)) {
+            System.out.printf("draw");
+        } else {
+            System.out.printf("%.4f", result);
+        }
     }
 }
