522d0c093843d7f3bfa57cda130d7679bfc022cc
Muxamch <Muxamch@gmail.com>
2020-07-13 21:56:43 +0300

    Update Main.java
    +input
    formatted output

--- Main.java
+++ Main.java
@@ -1,13 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        double votesA = 5;
-        double votesB = 5;
-        double difference = Double.max(votesA,votesB) - Double.min(votesA,votesB);
+        Scanner scan = new Scanner(System.in);
+
+        double votesB = scan.nextDouble();
+        double votesA = scan.nextDouble();
+
+        double difference = Double.max(votesA, votesB) - Double.min(votesA, votesB);
         double result = 100 / (votesA + votesB) * difference;
         if (result == 50 || (votesA == votesB)) {
             System.out.printf("draw");
+        } else if (votesA > votesB) {
+            System.out.printf("A %.4f%%", result);
         } else {
-            System.out.printf("%.4f", result);
+            System.out.printf("B %.4f%%", result);
         }
     }
 }
